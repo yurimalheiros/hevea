499f3531eb23be41313e97b9067e4b9ca56388db
yurimalheiros <yurimalheiros@gmail.com>
2013-01-05 15:02:30 -0300

    handling parameters and generating Makefile

--- hevea/handler.py
+++ hevea/handler.py
@@ -1,14 +1,18 @@
 import os
 import subprocess
+from string import Template
 from watchdog.events import FileSystemEventHandler
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
 class ChangeHandler(FileSystemEventHandler):
     """
     React to changes in .tex files
     """
 
-    def __init__(self, directory):
+    def __init__(self, directory, mainfile):
         self.directory = directory
+        self.mainfile = mainfile
 
     def on_any_event(self, event):
         """If any file or folder is changed"""
@@ -22,8 +26,17 @@ class ChangeHandler(FileSystemEventHandler):
     def make(self):
         print "Compiling..."
 
+        if not os.path.exists(os.path.join(self.directory, 'Makefile')):
+            makefile = open(os.path.join(BASEDIR, 'Makefile'))
+            template = Template(makefile.read())
+            makefile.close()
+
+            new_makefile = open(os.path.join(self.directory, 'Makefile'), 'w')
+            new_makefile.write(template.substitute(mainfile=self.mainfile, bibtexfile=self.mainfile))
+            new_makefile.close()
+
         FNULL = open(os.devnull, 'w')
-        os.chdir(os.path.join(self.directory))
+        os.chdir(self.directory)
         subprocess.call(r'make', stdout=FNULL)
 
     def _get_extension(self, filepath):
